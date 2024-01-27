254588afa318d66c24253b6d04e18927e9e42e12
Agshin N. Valiyev <agshinnv@code.edu.az>
2024-01-28 00:09:47 +0400

    js introduction practice added

---
+++ practice/assets/js/script.js
@@ -0,0 +1,174 @@
+"use strict";
+
+
+
+// var surname = "Huseynov";
+
+// console.log(surname);
+
+// // let address = "test";
+
+
+
+// let address = "Ehemedli";
+// console.log(address);
+
+// const age = 27;
+
+// console.log(age);
+
+// const name = "test";
+
+// name = "salam"
+
+// console.log(name);
+
+// let surname;
+
+// console.log(surname);
+
+// let isMarried = true;
+
+// isMarried = 100;
+
+// console.log(isMarried);
+
+
+// let price = 100;
+
+// price+=50;
+
+// console.log(price);
+
+// const stu = {
+//     name:"Agshin",
+//     surname:"Valiyev",
+//     age: 27
+// };
+
+// console.log(stu.name + "-" + stu.surname + "-" + stu.age);
+
+// // stu=null;
+
+// stu.name = "Test";
+
+
+// console.log(stu.name + "-" + stu.surname + "-" + stu.age);
+
+
+// let name = "Agshin";
+// let surname = "Valiyev";
+
+// let fullName = name + "-" + surname;
+
+// let fullName = `${name} + ${surname};`
+
+// console.log(fullName);
+
+
+
+// let ages = [22,45,78,16,"salam"];
+
+// console.log(ages[ages.length-1]);
+
+// let num1 = 100;
+// let num2 = 50;
+
+// let result = num1 + num2;
+
+// console.log(result);
+
+// let num1 = 2**5;
+
+// console.log(num1);
+
+// let num1 = 5 % 3;
+// console.log(num1);
+
+// let num1 = 5;
+
+// num1++;
+
+// console.log(num1);
+
+// let num1 = "Salam";
+// let num2 = 55;
+
+// let result = num1 + num2;
+
+// console.log(num1===num2);
+
+
+// let number = 100;
+
+// if(number > 200){
+//     console.log("yes");
+// }else{
+//     console.log("no");
+// }
+
+// let number = 100;
+
+// // if(number > 90 && number < 120){
+// //     console.log("yes");
+// // }else{
+// //     console.log("no");
+// // }
+
+// let result = number == 100 ? "yes" : "no";
+
+// console.log(result);
+
+
+// let weekDay = 1;
+
+// switch (weekDay) {
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+        
+//         case 1:
+//             console.log("Monday");
+//             break;
+//     default:
+//         console.log("Not selected");
+//         break;
+// }
+
+
+
+// let isMarried = false;
+
+// if(isMarried){
+//     console.log("Yes");
+// }else{
+//     console.log("No");
+// }
+
+// let nums = [5,4,3,8,9,30];   
+
+// if(nums[nums.length-1] === 30){
+//     console.log(nums.length);
+// }else{
+//     console.log("Not equal");
+// }
+
+
+// let nums = [1,3,5,6,7,8];
+
+// for (let i = 0; i < nums.length; i++) {
+    
+//     console.log(i);
+    
+// }
+
+let nums = [1,3,5,6,7,8];
+let sum = 0;
+
+for (let i = 0; i < nums.length; i++) {
+    
+    sum+=i;
+    
+}
+
+console.log(sum);
